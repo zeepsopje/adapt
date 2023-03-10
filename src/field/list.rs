173c895bc40baad2b1fd024bc00649889917834b
Stefan Huynh <stefanhuynh98@gmail.com>
2023-03-11 00:36:50 +0100

    rename sub_fields to fields in list for consistency

--- src/field/list.rs
+++ src/field/list.rs
@@ -2,17 +2,17 @@ use super::FieldType;
 
 #[derive(Debug)]
 pub struct List {
-    pub sub_fields: Vec<FieldType>
+    pub fields: Vec<FieldType>
 }
 
 impl List {
     pub fn new() -> List {
         List {
-            sub_fields: Vec::new(),
+            fields: Vec::new(),
         }
     }
 
-    pub fn add_sub_field(&mut self, field: FieldType) {
-        self.sub_fields.push(field);
+    pub fn add_field(&mut self, field: FieldType) {
+        self.fields.push(field);
     }
 }
